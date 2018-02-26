5487ff34ef0feb22edc7821964218e6d57ec9a9f
Shohei Kameda <shoheik@cpan.org>
2018-02-26 09:48:48 +0900

    first one

---
+++ scripts/run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+useradd -m -d /home/$USERNAME -s /bin/bash -p $(echo $PASSWORD | openssl passwd -1 -stdin) $USERNAME
+chown -R $USERNAME:$USERNAME /home/$USERNAME
+
+# Enable user to sudo 
+usermod -aG sudo $USERNAME
+
+htpasswd -cb /etc/apache2/webdav.password $USERNAME $PASSWORD
+chown root:$USERNAME /etc/apache2/webdav.password
+chmod 640 /etc/apache2/webdav.password
+
+mkdir -p /var/tmp/run
+mkdir -p /var/tmp/lock/apache2
+mkdir -p /var/tmp/log/apache2
+
+chmod 777 -R /var/tmp
+
+# change superviser env variables
+sed -i -e "s|CHANGE_USERNAME_LATER|${USERNAME}|" /etc/default/devany.env
+sed -i -e "s|CHANGE_PASSWORD_LATER|${PASSWORD}|" /etc/default/devany.env
+
+# Run upervisord
+/etc/init.d/supervisor start
+
